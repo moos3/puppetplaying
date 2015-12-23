d8f40510115b3cb18a7f39f73c4e85b5ff71d9e7
root <root@ip-10-210-4-86.ec2.internal>
2015-12-23 07:17:17 -0500

    fixes to configuration files for kube and nginx

--- puppet/modules/webpage/manifests/init.pp
+++ puppet/modules/webpage/manifests/init.pp
@@ -14,8 +14,8 @@ class webpage{
     mode => 0644,
   }
 
-  file{'/var/www/html/images/looppay.png':
-    content => 'puppet:///modules/webpage/looppay.png',
+  file{'/var/www/html/images/Logo_for_PR.jpg':
+    content => 'puppet:///modules/webpage/Logo_for_PR.jpg',
     owner => www-data,
     group => www-data,
     mode => 0644,
